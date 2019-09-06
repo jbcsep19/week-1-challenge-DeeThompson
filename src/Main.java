e73a0f78b25e6282b30db945bc46e572a6bed940
deethompson <devon.thompson0@gmail.com>
2019-09-06 15:09:34 -0400

    This is an update on the original

--- src/Main.java
+++ src/Main.java
@@ -9,19 +9,17 @@ public class Main {
         String Go, Look = null, Walk = null, Eat, Lay, Open = null;
 
 
-
         Scanner key = new Scanner(System.in);
 
         System.out.println("This is your choose your adventure  you can Go in the 'First Floor' or 'Upstairs");
         System.out.println("If you choose to Go to 'First Floor' you can Go into the 'Kitchen', 'Living Room ', 'Patio");
         System.out.println("You can also Go to 'Basement' and 'walk in the dark'");
         System.out.println("If you Go 'Upstairs' you can go to the 'Bedroom' or 'Bathroom' ");
-        System.out.println("Where do you want to Go?");
-        Go = key.next();
+        System.out.println("Where do you want to Go?");Go = key.next();
 
-        if (Go.equalsIgnoreCase ("First Floor")) {
-            System.out.println ("Do you want to go to the Kitchen or Upstairs?");
-            System.out.print (">");
+        if (Go.equalsIgnoreCase("First Floor")) {
+            System.out.println("Do you want to go to the Kitchen or Upstairs?");
+            System.out.print(">");
             Walk = key.next();
 
             if (Walk.equalsIgnoreCase("Kitchen")) {
@@ -31,84 +29,55 @@ public class Main {
                 System.out.println(">");
                 System.out.println("Bedroom or Bathroom");
                 System.out.println(">");
+                Look = key.next();
 
+
+                if (Look.equalsIgnoreCase("refrigerator")) ;
+                System.out.println("In side the refrigerator is ' Strange Milk and 'Leftovers' Do you want to drink the milk or eat leftovers?");
+                System.out.print(">");
+                Eat = key.next();
+
+                if (Eat.equalsIgnoreCase("Drink Milk")) {
+                    System.out.println(" ");
+                    System.out.println("You will throw up");
+                } else if (Eat.equalsIgnoreCase("eat leftovers")) {
+                    System.out.println(" ");
+                    System.out.println("You will still throw up");
+                }
+
+                if (Walk.equalsIgnoreCase("Bedroom")) {
+                    System.out.println("Lay on bed");
+                } else if (Walk.equalsIgnoreCase("Open the drawer")) {
+                    System.out.println(" ");
+                    System.out.println("Ewwwww Dirty underwear!");
+                    System.out.println("Now do you want to go back to the First Floor? Yes or No?");
+                } else if (Walk.equalsIgnoreCase("Yes")) {
+                    System.out.println(" ");
+                    System.out.println("Go to Patio");
+                } else if (Walk.equalsIgnoreCase("No")) {
+                    System.out.println(" ");
+                    System.out.println("Go to Living Room");
+                    Walk = key.next();
+
+                    if (Walk.equalsIgnoreCase("Turn on the TV")) {
+                        System.out.print(" ");
+                        System.out.print("Why is the TV so loud!?");
+                        System.out.println("I need to turn the volume down"); }
+
+                    if (Go.equalsIgnoreCase("Basement")) {
+                        System.out.print("");
+                        System.out.print("It's Dark I can't see anything down here!");
+                        System.out.print("Do you want to turn on the lights or leave?");
+                    } else if (Walk.equalsIgnoreCase("turn on the lights")) {
+                      System.out.println("");
+                      System.out.println("Open a closet");
+                      System.out.println("Surprise!! You don't get to leave you have to clean the entire house, Here's cleaning supplies");
+                      System.out.println("The End!! Have fun scrubbing the toilets");
+
+
+                    }
+                }
             }
-
-
         }
-
-        if  (Look == "refrigerator");
-            System.out.println("In side the refrigerator is ' Strange Milk and 'Leftovers' Do you want to drink the milk or eat leftovers?");
-            System.out.print (">");
-            Eat=key.next();
-
-            if (Eat.equalsIgnoreCase ("Drink Milk")) {
-                System.out.println(" ");
-                System.out.println("You will throw up");
-            }else if (Eat.equalsIgnoreCase("eat leftovers")) {
-                System.out.println(" ");
-                System.out.println("You will still throw up");
-            }
-            if (Go == "Upstairs")
-                System.out.println ("Go into the bedroom");
-            if (Go == "Bedroom")
-                System.out.println ("Lay on bed");
-            else if (Open =="Open the  drawer" );
-
-            if (Go == "Bathroom")
-                System.out.println (" ");
-            System.out.print("I need to pee");
-             if (Go == "Use the toilet");
-            System.out.println (" ");
-            System.out.print("water shoots out the toilet and you run out the bathroom");
-
-            if (Walk == "Patio")
-                System.out.println (" ");
-            System.out.print ("Maybe I'll go to the pool");
-
-            if (Look == "the pool");
-            System.out.println (" ");
-            System.out.print ("This pool is dirty! But I want to Swim");
-
-            if ( Go == "Swim in the pool");
-            System.out.println (" ");
-            System.out.print ("Need to clean the pool");
-
-            System.out.println ("Gets out of pool and dries off");
-
-            if (Go == "Living Room" );
-            System.out.println (" ");
-            System.out.print ("relax 'Turn on the TV'");
-            System.out.print ("Change the change");
-
-            if (Go == "Change channel");
-            System.out.println (" ");
-            System.out.print ("The Volume increased on it's own WTH!? Do you want to decrease the volume? Yes or No?");
-
-            if (Go  == " yes Decrease the volume");
-            System.out.println (" ");
-            System.out.print ("your ears have been saved, do you want to continue watching TV?");
-
-            if (Go == "no Decrease the volume");
-            System.out.println (" ");
-            System.out.print ("The neighbors will call the cops and your eardrums will burst");
-
-            if (Walk == "To the 'Basement'");
-            System.out.println (" ");
-            System.out.print ("Turn on the lights");
-
-            if (Walk == "Open a closet");
-            System.out.println ("");
-            System.out.print ("Surprise!! Here are plane tickets to Tahiti and a week hotel stay, you made it through the house!");
-
-
-
-
-
     }
-
-    private static void If(boolean b) {
-    }
-
-
 }
